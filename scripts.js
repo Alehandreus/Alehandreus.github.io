bd10a54715a23608b38ce11f33dddf28dc422677
Alehandreus <alehandro.nikolaev@gmail.com>
2021-07-29 15:17:12 +0300

    Some links added

--- scripts.js
+++ scripts.js
@@ -9,3 +9,41 @@ function copyToClipboard() {
     document.execCommand('copy');
     document.body.removeChild(el);
 };
+
+window.onload = function createLinks() {
+    var linksData = [
+        ["simple-icons:facebook", "Facebook", "https://www.facebook.com/profile.php?id=100024930003858"],
+        ["simple-icons:vk", "ВКонтакте", "https://vk.com/id474339941"],
+        ["simple-icons:steam", "Steam", "https://steamcommunity.com/profiles/76561198854382470/"],
+        ["simple-icons:github", "GitHub", "https://github.com/Alehandreus"],
+        ["simple-icons:telegram", "Telegram", "https://t.me/alehandreus"],
+        // ["cib:kaggle", "Kaggle", "https://www.kaggle.com/alehandreus"], not yet
+        // ["simple-icons:leetcode", "Leetcode", "https://leetcode.com/Alehandreus/"], not yet
+    ]
+    linksData.forEach(element => {
+        createLink(element[0], element[1], element[2]);
+    });
+    
+}
+
+function createLink(iconData, linkName, linkUrl) {
+    linkContainer = $("<a>");
+    linkContainer.addClass("link-container");
+    linkContainer.attr("href", linkUrl);
+
+    icon = $("<span>");
+    icon.addClass("iconify link-icon");
+    icon.attr("data-icon", iconData);
+    icon.attr("data-inline", "False");
+    icon.attr("data-width", 37);
+    icon.attr("data-height", 37);
+
+    linkText = $("<div>");
+    linkText.addClass("link-text");
+    linkText.text(linkName);
+
+    linkContainer.append(icon);
+    linkContainer.append(linkText);
+
+    $('#links').append(linkContainer);
+}
